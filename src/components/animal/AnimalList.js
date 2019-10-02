51e2d4794cbce22e0daac5247ac71e41a79cd442
unknown <[shrestha_sl@hotmail.com]>
2019-10-02 13:51:50 -0500

    animal edit form and feature added for editing the animal

--- src/components/animal/AnimalList.js
+++ src/components/animal/AnimalList.js
@@ -48,7 +48,7 @@ class AnimalList extends Component {
 				</section>
 				<div className='container-cards'>
 					{this.state.animals.map(animal => (
-						<AnimalCard key={animal.id} animal={animal} deleteAnimal={this.deleteAnimal} />
+						<AnimalCard key={animal.id} animal={animal} deleteAnimal={this.deleteAnimal} {...this.props}/>
 					))}
 				</div>
 			</>
