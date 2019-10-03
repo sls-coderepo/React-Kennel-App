8cae9ef57fe30981c94e2ccb0cf797fc04ddebf4
unknown <[shrestha_sl@hotmail.com]>
2019-10-03 15:36:20 -0500

    login and logout button added

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -26,6 +26,16 @@ class Kennel extends Component {
     });
   }
 
+  clearUser = () => {
+    localStorage.clear()
+
+    this.setState({
+        user: this.isAuthenticated()
+    });
+
+}
+
+
   componentDidMount(){
     this.setState({
       user: this.isAuthenticated()
@@ -35,7 +45,7 @@ class Kennel extends Component {
 render() {
   return (
     <>
-      <NavBar user={this.state.user} />
+      <NavBar user={this.state.user} clearUser={this.clearUser} />
       <ApplicationViews user={this.state.user}
                         setUser={this.setUser} />
     </>
--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
-import { Link } from "react-router-dom"
+import { Link, withRouter} from "react-router-dom"
 import './NavBar.css'
 
 class NavBar extends Component {
+  handleLogout = () => {
+    this.props.clearUser();
+    this.props.history.push('/');
+}
 
   render(){
 
@@ -22,11 +26,10 @@ class NavBar extends Component {
                 <>
                 <li><Link className="nav-link" to="/employees">Employees</Link></li>
                 <li>Owners</li>
-                </>
-                : null }
-                {/* <li><span className="nav-link" onClick={this.handleLogout}>Logout</span></li> */}
-            
-                <li><Link className="nav-link" to="/login">Login</Link></li>
+                
+                 <li><span className="nav-link" onClick={this.handleLogout}>Logout</span></li>
+                 </>
+               : <li><Link className="nav-link" to="/login">Login</Link></li>
             }
           </ul>
         </nav>
@@ -35,4 +38,4 @@ class NavBar extends Component {
   }
 }
 
-export default NavBar;
+export default withRouter(NavBar);
